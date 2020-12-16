d9caeedeeb3d25a35020b0f60ac86088fb637fc4
mzw86020424 <mzw86020424@gmail.com>
2020-12-16 18:14:28 +0900

    テスト用のjsファイル作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("./test.js") //テスト用
 
 import 'bootstrap';
 import '../stylesheets/application';
