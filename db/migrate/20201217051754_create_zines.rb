2aea413fb90a559f85e2302e4692637dce825a4b
mzw86020424 <mzw86020424@gmail.com>
2020-12-19 13:07:28 +0900

    zineテーブルのカラム修正

--- db/migrate/20201217051754_create_zines.rb
+++ db/migrate/20201217051754_create_zines.rb
@@ -1,7 +1,7 @@
 class CreateZines < ActiveRecord::Migration[6.0]
   def change
     create_table :zines do |t|
-      t.boolean    :release, default: false, null: false
+      t.boolean    :private, default: true, null: false
       t.string     :year,    null: false
       t.string     :month,   null: false
       t.references :user,    null: false, foreign_key: true
