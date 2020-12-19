24e423b426208e9d0830b88c2926a08ef2c8c674
mzw86020424 <mzw86020424@gmail.com>
2020-12-19 14:16:09 +0900

    非同期通信の公開・非公開チェックボックスを設置

--- app/controllers/zines_controller.rb
+++ app/controllers/zines_controller.rb
@@ -1,6 +1,5 @@
 class ZinesController < ApplicationController
 
-
   def show
     @zines = Zine.where(user_id: current_user.id)
     @zine = Zine.find(params[:id])
@@ -8,4 +7,14 @@ class ZinesController < ApplicationController
     @posts = @zine.posts
   end
 
+  def user_private
+  @zine = Zine.find(params[:id])
+  @zine.private = !@zine.private # privates でなく private かも
+  @zine.save #ここでuser_params使わないの？？？
+end
+
+private
+def user_params
+    params.require(:zine).permit(:private)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   root "posts#index"
-  resources :zines
+  resources :zines do
+    collection do
+	  post '/:id/user_private' => 'zines#user_private'
+    end
+  end
   resources :users, only: [:edit, :update]
 end
