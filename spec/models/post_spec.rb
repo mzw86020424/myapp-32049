7897c7fa4fbf287ffe3db2926c12f31c97a5190e
mzw86020424 <mzw86020424@gmail.com>
2020-12-22 12:42:15 +0900

    Postモデルの単体テストコードを実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,65 +5,27 @@ describe Post do
   end
 
   describe 'URL新規投稿' do
-    context '新規登録がうまくいくとき' do
-      it "nickname、email、画像、passwordとpassword_confirmationが存在すれば登録できる" do
-        expect(@post).to be_valid
-      end
-      it "nicknameが6文字以下で登録できる" do
-        @post.nickname = '12345'
-        expect(@post).to be_valid
-      end
-      it "passwordが6文字以上であれば登録できる" do
-        @post.password = '123456'
-        @post.password_confirmation = "123456"
+    context '新規投稿がうまくいくとき' do
+      it "URL、year、monthが存在すれば登録できる" do
         expect(@post).to be_valid
       end
     end
 
     context '新規登録がうまくいかないとき' do
-      it "nicknameが空だと登録できない" do
-        @post.nickname = ""
+      it "URLが空だと登録できない" do
+        @post.url = ""
         @post.valid?
-        expect(@post.errors.full_messages).to include "Nickname can't be blank"
+        expect(@post.errors.full_messages).to include "Url can't be blank"
       end
-      it "nicknameが7文字以上であれば登録できない" do
-        @post.nickname = "1234567"
+      it "USERが紐づいていないと登録できない" do
+        @post.user = nil
         @post.valid?
-        expect(@post.errors.full_messages).to include "Nickname is too long (maximum is 6 characters)"
+        expect(@post.errors.full_messages).to include "User must exist"
       end
-      it "emailが空では登録できない" do
-        @post.email = ""
+      it "ZINEが紐づいていないと登録できない" do
+        @post.zine = nil
         @post.valid?
-        expect(@post.errors.full_messages).to include "Email can't be blank"
-      end
-      it "重複したemailが存在する場合登録できない" do
-        @post.email = "aaa@aaa"
-        @post.save
-        @post2 = FactoryBot.build(:post)
-        @post2.email = "aaa@aaa"
-        @post2.valid?
-        expect(@post2.errors.full_messages).to include "Email has already been taken"
-      end
-      it "passwordが空では登録できない" do
-        @post.password = ""
-        @post.valid?
-        expect(@post.errors.full_messages).to include "Password can't be blank"
-      end
-      it "passwordが5文字以下であれば登録できない" do
-        @post.password = "12345"
-        @post.password_confirmation = "12345"
-        @post.valid?
-        expect(@post.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
-      end
-      it "passwordが存在してもpassword_confirmationが空では登録できない" do
-        @post.password_confirmation = ""
-        @post.valid?
-        expect(@post.errors.full_messages).to include "Password confirmation doesn't match Password"
-      end
-      it "画像が空では登録できない" do
-        @post.image = nil
-        @post.valid?
-        expect(@post.errors.full_messages).to include "Image can't be blank"
+        expect(@post.errors.full_messages).to include "Zine must exist"
       end
     end
   end
